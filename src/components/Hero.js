06c8601b7a083c1e3bbdee907c11e4ca1535aa07
SherieBach <Sherie.klappenbach@gmail.com>
2021-06-04 09:40:06 +0200

    added img attribution and minor color changes to btn

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,6 +10,8 @@ const Hero = () => {
                     <h1 className="hero-title">Unsplash image search</h1>
                     <p>Find amazing picture from all around the world</p>
                 </div>
+                <div className="hero-attribution">Photo by <a href="https://unsplash.com/@lucadgr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Luca Dugaro</a> on <a href="https://unsplash.com/s/photos/clouds?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
             </div>
         </>
     );
