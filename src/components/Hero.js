1aec21a66cb839757a754dcc60bf45bfee93d1c3
SherieBach <Sherie.klappenbach@gmail.com>
2021-06-04 13:50:55 +0200

    fixed typo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
             <div className="hero">
                 <div className="hero-content">
                     <h1 className="hero-title">Unsplash image search</h1>
-                    <p>Find amazing picture from all around the world</p>
+                    <p>Find amazing pictures from all around the world</p>
                 </div>
                 <div className="hero-attribution">Photo by <a href="https://unsplash.com/@lucadgr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Luca Dugaro</a> on <a href="https://unsplash.com/s/photos/clouds?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
