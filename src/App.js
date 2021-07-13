7a280285571376e6d62331282c836dbb13ddc5b1
SherieBach <Sherie.klappenbach@gmail.com>
2021-07-13 15:57:12 +0200

    config added base64 string

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Hero />
+                <Hero/>
                 <SearchImage/>
             </div>
         )
